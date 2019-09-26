ac191c2bbb93abd071852fb9b405fe24b5befbb6
wangyong <vangyong@126.com>
2019-09-26 14:03:13 +0800

    修改

--- src/pcap_util.go
+++ src/pcap_util.go
@@ -1,16 +1,16 @@
 package main
- 
+
 import (
 	"fmt"
 	"os"
 	"time"
- 
+
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 	"github.com/google/gopacket/pcapgo"
 )
- 
+
 var (
 	deviceName  string = "eth0"
 	snapshotLen int32  = 1024
@@ -20,14 +20,14 @@ var (
 	handle      *pcap.Handle
 	packetCount int = 0
 )
- 
+
 func main() {
-	// Open output pcap file and write header 
+	// Open output pcap file and write header
 	f, _ := os.Create("test.pcap")
 	w := pcapgo.NewWriter(f)
 	w.WriteFileHeader(snapshotLen, layers.LinkTypeEthernet)
 	defer f.Close()
- 
+
 	// Open the device for capturing
 	handle, err = pcap.OpenLive(deviceName, snapshotLen, promiscuous, timeout)
 	if err != nil {
@@ -35,7 +35,7 @@ func main() {
 		os.Exit(1)
 	}
 	defer handle.Close()
- 
+
 	// Start processing packets
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
@@ -43,7 +43,10 @@ func main() {
 		fmt.Println(packet)
 		w.WritePacket(packet.Metadata().CaptureInfo, packet.Data())
 		packetCount++
-		
+
 		// Only capture 100 and then stop
 		if packetCount > 100 {
 			break
+		}
+	}
+}
